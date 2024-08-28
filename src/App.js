4b238b7bab6dc40de6d159b62359d43542d79846
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-08-28 22:43:23 +0530

    user app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddUsers from './components/AddUsers';
+
+function App() {
+  return (
+    <div>
+      <AddUsers/>
+    </div>
+  );
+}
+
+export default App;
